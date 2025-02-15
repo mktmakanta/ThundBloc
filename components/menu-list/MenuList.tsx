f2db31cf085f0a2b0d75f28f6f8c896e545fd4c6
mktmakanta <mktmakantamkt@gmail.com>
2025-02-15 01:30:10 +0100

    i have just finshed the nav menus

--- components/menu-list/MenuList.tsx
+++ components/menu-list/MenuList.tsx
@@ -4,6 +4,7 @@ import { closeMenu, setActiveCategory } from "@/providers/menuSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/providers/store";
 import { useEffect } from "react";
+import MobileNav from "./MobileList";
 
 const menuItems = [
   {
@@ -37,64 +38,68 @@ const MenuList = () => {
   return (
     <>
       {isOpen && (
-        <main className="bg-yellow-500 relative h-screen w-full z-50 overflow-y-scroll ">
-          <div
-            className={` md:h-screen w-full h-screen grid  grid-rows-[60px,1fr,1fr]  md:grid-rows-[100px,1fr,80px] `}
-          >
-            {/* top one  */}
-            <div className="bg-neutral-950 border-b border-neutral-600 text-white flex justify-end items-center space-x-5 font-semibold font-ibm px-10">
-              <h2 className="hidden lg:flex">EXPLORE</h2>
-              <button onClick={() => dispatch(closeMenu())} className="">
-                CLOSE
-              </button>
-            </div>
-            {/* mobile middle */}
-            <div className="md:hidden h-full w-full "></div>
-            {/* tablet and desktop middle */}
-            <div className="hidden md:grid grid-cols-2">
-              <div className="grid grid-rows-3 ">
-                {menuItems.map((item) => (
-                  <div
-                    key={item.category}
-                    className={`p-14 text-7xl flex items-center border-y border-neutral-600 font-medium cursor-pointer transition-all ${
-                      activeCategory === item.category
-                        ? "bg-neutral-100 border-r border-black text-black"
-                        : "bg-neutral-950 text-white"
-                    }`}
-                    onMouseEnter={() =>
-                      dispatch(setActiveCategory(item.category))
-                    }
-                    // onMouseLeave={() => setActiveCategory(null)}
-                  >
-                    {item.category}
-                  </div>
-                ))}
+        <main>
+          {/* mobile  */}
+          <MobileNav />
+          {/* desktop  */}
+          <div className="hidden md:block relative h-screen w-full z-50  ">
+            <div
+              className={` md:h-screen w-full h-screen grid grid-rows-[100px,1fr,80px] `}
+            >
+              {/* top one  */}
+              <div className="bg-neutral-950 border-b border-neutral-600 text-white flex justify-end items-center space-x-5 font-semibold font-ibm px-10">
+                <h2 className="hidden lg:flex">EXPLORE</h2>
+                <button onClick={() => dispatch(closeMenu())} className="">
+                  CLOSE
+                </button>
               </div>
 
-              <div className=" bg-neutral-100 py-10 px-14 space-y-8">
-                {menuItems
-                  .find((item) => item.category === activeCategory)
-                  ?.submenu.map((subItem, index) => (
+              {/* tablet and desktop middle */}
+              <div className="hidden md:grid grid-cols-2">
+                <div className="grid grid-rows-3 ">
+                  {menuItems.map((item) => (
                     <div
-                      key={index}
-                      className="text-5xl font-medium text-gray-500 py-2 hover:text-black cursor-pointer "
+                      key={item.category}
+                      className={`p-14 text-7xl flex items-center border-y border-neutral-600 font-medium cursor-pointer transition-all ${
+                        activeCategory === item.category
+                          ? "bg-neutral-100 border-r border-black text-black"
+                          : "bg-neutral-950 text-white"
+                      }`}
+                      onMouseEnter={() =>
+                        dispatch(setActiveCategory(item.category))
+                      }
+                      // onMouseLeave={() => setActiveCategory(null)}
                     >
-                      {subItem}
+                      {item.category}
                     </div>
                   ))}
-              </div>
-            </div>
+                </div>
 
-            {/* last one  */}
-            <div className="bg-neutral-950 text-white border-t md:space-x-96 font-ibm text-sm font-semibold flex flex-col md:flex-row px-14 items-center border-neutral-600 overflow-hidden ">
-              <div className=""> ©2025 ThundBloc</div>
-              <ul className="space-x-8 flex cursor-pointer ">
-                <li>X</li>
-                <li>Discord</li>
-                <li>Telegram</li>
-                <li>GitHub</li>
-                <li>Reddit</li>
-              </ul>
+                <div className=" bg-neutral-100 py-10 px-14 space-y-8">
+                  {menuItems
+                    .find((item) => item.category === activeCategory)
+                    ?.submenu.map((subItem, index) => (
+                      <div
+                        key={index}
+                        className="text-5xl font-medium text-gray-500 py-2 hover:text-black cursor-pointer "
+                      >
+                        {subItem}
+                      </div>
+                    ))}
+                </div>
+              </div>
+
+              {/* last one  */}
+              <div className="bg-neutral-950 text-white border-t space-x-96 font-ibm text-sm font-semibold flex flex-col md:flex-row px-14 items-center border-neutral-600 overflow-hidden ">
+                <div className=""> ©2025 ThundBloc</div>
+                <ul className="space-x-8  flex flex-row cursor-pointer ">
+                  <li>X</li>
+                  <li>Discord</li>
+                  <li>Telegram</li>
+                  <li>GitHub</li>
+                  <li>Reddit</li>
+                </ul>
+              </div>
             </div>
           </div>
         </main>
--- components/menu-list/MobileList.tsx
+++ components/menu-list/MobileList.tsx
@@ -1,7 +1,95 @@
-import React from "react";
+import { useState } from "react";
+import { ChevronDown, X } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/providers/store";
+import { closeMenu } from "@/providers/menuSlice";
 
-const MobileList = () => {
-  return <div>MobileList</div>;
-};
+const menuItems = [
+  {
+    category: "Products",
+    submenu: [
+      "Gaia Domain",
+      "AI Agent Domains",
+      "LLM Library",
+      "Gaia XP",
+      "Network Map",
+      "Chat",
+    ],
+  },
+  {
+    category: "Developers",
+    submenu: ["Docs", "Use Cases", "Ecosystem", "Install Node"],
+  },
+  { category: "Community", submenu: ["Blogs", "Events", "FAQs"] },
+];
 
-export default MobileList;
+export default function MobileNav() {
+  const dispatch = useDispatch();
+  const { isOpen } = useSelector((state: RootState) => state.menu);
+
+  const [openCategory, setOpenCategory] = useState(null);
+
+  const toggleCategory = (category) => {
+    setOpenCategory(openCategory === category ? null : category);
+  };
+
+  return (
+    <div className="md:hidden relative">
+      {isOpen && (
+        <div className="fixed inset-0 bg-neutral-950 text-white w-full h-full z-40  overflow-y-auto">
+          <div className=" sticky top-0 z-50 bg-neutral-950 flex justify-between items-center px-4 py-6  border-b border-neutral-600">
+            <h2 className=" font-bold">THUNDBLOC</h2>
+            <button
+              onClick={() => dispatch(closeMenu())}
+              className="font-ibm text-sm font-semibold"
+            >
+              CLOSE
+            </button>
+          </div>
+          <nav>
+            {menuItems.map((item, index) => (
+              <div key={index} className="border-b border-gray-700">
+                <button
+                  className={`w-full flex justify-between items-center py-6 px-4 text-left text-3xl font-semibold ${
+                    openCategory === item.category ? "bg-white text-black" : ""
+                  }`}
+                  onClick={() => toggleCategory(item.category)}
+                >
+                  <div className="text-5xl"> {item.category}</div>
+                  <ChevronDown
+                    className={`transition-transform duration-300 ${
+                      openCategory === item.category ? "rotate-180" : ""
+                    }`}
+                  />
+                </button>
+                {openCategory === item.category && (
+                  <ul className=" space-y-4 text-3xl py-5 pl-3 bg-white ">
+                    {item.submenu.map((submenuItem, subIndex) => (
+                      <li
+                        key={subIndex}
+                        className="pl-4 font-semibold text-black"
+                      >
+                        {submenuItem}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+          </nav>
+          <ul className=" space-y-4 px-4 py-8 text-sm font-semibold font-ibm cursor-pointer ">
+            <li>X</li>
+            <li>Discord</li>
+            <li>Telegram</li>
+            <li>GitHub</li>
+            <li>Reddit</li>
+          </ul>
+          <div className="px-4 pb-6 pt-10 flex justify-between items-center ">
+            <p className="font-ibm text-sm">©2025 ThundBloc</p>
+            <div className="size-10 rounded-lg bg-white"></div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
